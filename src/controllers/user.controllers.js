495bf84b8e82c0625f21b50c61a67c19a2e1c188
TheSaiTeja <iamsaiteja2411@gmail.com>
2025-10-16 19:05:43 +0530

    feat: update Prisma client and schema to version 6.17.1, implement user update functionality with validation and logging

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ import { PrismaClient } from '../generated/prisma/client.js'
 import { asyncHandler } from '../utils/asyncHandler.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { ApiError } from '../utils/ApiError.js'
+import { logDebug, logWarn } from '../utils/logger.js'
 
 const prisma = new PrismaClient().$extends(withAccelerate())
 
@@ -32,4 +33,68 @@ export const getUser = asyncHandler(async (req, res) => {
 	}
 
 	res.status(200).json(new ApiResponse(200, user, 'User fetched successfully'))
+})
+
+export const updateUser = asyncHandler(async (req, res) => {
+	const userId = req.params.id // get user id from params
+	const updates = req.body // get fileds to update from body
+
+	// Input validation
+	if (!userId) {
+		logWarn('User id is required')
+		throw new ApiError(400, 'User ID is required')
+	}
+
+	logDebug('updates logged', updates)
+
+	const allowedFields = ['firstName', 'lastName', 'dateOfBirth', 'height', 'weight']
+	const fieldsToUpdate = {}
+
+	// Check if user exists
+	const existingUser = await prisma.user.findUnique({
+		select: {
+			id: true,
+			firstName: true,
+			lastName: true,
+			dateOfBirth: true,
+			height: true,
+			weight: true,
+		},
+		where: { id: userId },
+	})
+
+	if (!existingUser) {
+		logWarn('User does not exist', { action: 'findUser', userId })
+		throw new ApiError(404, 'User does not exist')
+	}
+
+	// Filter only allowed fields
+	for (const field of allowedFields) {
+		if (updates[field] !== undefined && updates[field] !== null && updates[field] !== '') {
+			fieldsToUpdate[field] = updates[field]
+		}
+	}
+
+	if (Object.keys(fieldsToUpdate).length <= 0) {
+		logWarn('No valid fields provided for updating', { action: 'updateUser' })
+		throw new ApiError(404, 'No valid fields provided for update')
+	}
+
+	const updatedUser = await prisma.user.update({
+		where: { id: userId },
+		data: {
+			...fieldsToUpdate,
+			dateOfBirth: fieldsToUpdate.dateOfBirth ? new Date(fieldsToUpdate.dateOfBirth) : existingUser.dateOfBirth,
+		},
+	})
+
+	logDebug('Converting to ISO string', {
+		action: 'dateOfBirthToISOString',
+		data: {
+			...fieldsToUpdate,
+			dateOfBirth: fieldsToUpdate.dateOfBirth ? new Date(fieldsToUpdate.dateOfBirth) : existingUser.dateOfBirth,
+		},
+	})
+
+	return res.status(200).json(new ApiResponse(200, updatedUser, 'Details updated successfully'))
 })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { getUser } from '../controllers/user.controllers.js'
+import { getUser, updateUser } from '../controllers/user.controllers.js'
 
 const router = Router()
 
 router.route('/:id').get(getUser)
+router.route('/:id').patch(updateUser)
 
 export const userRoutes = router
